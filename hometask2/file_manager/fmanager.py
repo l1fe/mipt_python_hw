581f1cdeae8a705a495b8ee51efc1458d6a3cca2
Igor Naumov <igor.naumov.17@gmail.com>
2014-03-23 21:10:32 +0400

    filemanager extention
    v1.0.2

--- hometask2/file_manager/fmanager.py
+++ hometask2/file_manager/fmanager.py
@@ -6,7 +6,7 @@ import shutil
 #Written by Igor Naumov MIPT 296
 #MIPT-Python 2014
 
-import sys, os, shutil
+import sys, os, shutil, platform
 
 class ExitCommand(object):
     def get_name(self):
@@ -79,6 +79,30 @@ class RemoveCommand(object):
         ShellUtils.check_args_num(self, len(args))
 
         state.file_manager.remove(args[0])
+
+class ClearScreenCommand(object):
+    def get_name(self):
+        return 'clear'
+    def get_args_num(self):
+        return 1
+    def execute(self, args_line, state):
+        args = ShellUtils.parse_cmd_params(args_line)
+        #print('Args: {}'.format(args))
+        ShellUtils.check_args_num(self, len(args))
+
+        state.file_manager.remove(args[0])
+
+class LsCommand(object):
+    def get_name(self):
+        return 'ls'
+    def get_args_num(self):
+        return 1
+    def execute(self, args_line, state):
+        args = ShellUtils.parse_cmd_params(args_line)
+        #print('Args: {}'.format(args))
+        ShellUtils.check_args_num(self, len(args))
+
+        state.file_manager.show_content(args[0])
 
 class InvalidArgumentException(Exception):
     def __init__(self, value):
@@ -174,9 +198,10 @@ class FileManager(object):
         self.current_dir = os.getcwd()
 
     def set_current_dir(self, new_dir):
-        self.current_dir = os.path.join(self.current_dir, new_dir)
-        if n
-        return 0
+        new_dir = os.path.join(self.current_dir, new_dir)
+        if not os.path.exists(new_dir) or not os.path.isdir(new_dir):
+            raise InvalidArgumentException('Directory {} does not exist'.format(new_dir))
+        self.current_dir = new_dir
 
     def make_dir(self, dir_name):
         if os.path.exists(dir_name) and os.path.isdir(dir_name):
@@ -184,6 +209,13 @@ class FileManager(object):
 
         os.mkdir(os.path.join(self.current_dir, dir_name))
         print('Created', os.path.join(self.current_dir, dir_name))
+
+    def show_content(self, dir_name):
+        dir_name = os.path.join(self.current_dir, dir_name)
+        if not os.path.exists(dir_name) or not os.path.isdir(dir_name):
+            raise InvalidArgumentException('Directory {} does not exist'.format(dir_name))
+
+        print('\n'.join(os.listdir(dir_name)))
 
     def remove(self, file_n):
         return 0
@@ -199,5 +231,7 @@ shell.add_cmd(MkdirCommand())
 shell.add_cmd(CopyCommand())
 shell.add_cmd(MoveCommand())
 shell.add_cmd(RemoveCommand())
+shell.add_cmd(LsCommand())
+shell.add_cmd(ClearScreenCommand())
 
 shell.run()
